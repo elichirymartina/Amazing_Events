25cf60bc408b38c43bcf7a6b41c13278bffba4a9
Marti E <elichirymartina@gmail.com>
2023-03-17 13:16:22 -0300

    tables

--- details.js
+++ details.js
@@ -1,57 +1,57 @@
-$(document).ready(function(){
-  alert('Hello! I am Working!')
-});
+const cargarEventos = async() => {
+  try {
+      const respuesta = await fetch('https://mindhub-xj03.onrender.com/api/amazing');
+      //const respuesta = await fetch('data.json');
+  console.log(respuesta);
 
-const queryString = location.search
+  const datos = await respuesta.json();
+  console.log(datos);
 
-const params = new URLSearchParams(queryString)
+  //detalles
 
-const id = params.get("id")
+  const queryString = location.search
 
-const cartas = data.events.find(evento => evento._id == id)
-console.log(cartas)
+  const params = new URLSearchParams(queryString)
 
+  const id = params.get("id")
 
+  const cartas = datos.events.find(evento => evento._id == id)
+  console.log(cartas)
 
-let contenedor = document.getElementById("card_details");
+  let contenedor = document.getElementById("card_details");
 
-
-
-
-function createCard(carta, contenedor) {
+  function createCard(carta, contenedor) {
   let div = document.createElement('div');
   div.classList.add("detalles")
   div.innerHTML = 
-
   `
-  
-  <table "table table-bordered" width=60% height=70% >
+  <table class="table table-bordered" style="width:100%">
   <tr class="titulo-tabla">
-    <th colspan="3" class="h2"> Event Name: </th>
-    <td colspan="3" class="h2"> ${carta.name}</td>
+    <th class="h2"> Event Name: </th>
+    <td class="h2"> ${carta.name}</td>
   </tr>
   <tr>
     <th class="table-light"> Event Description: </th>
-    <td colspan="3" class="table-light">${carta.description}</td>
+    <td class="table-light">${carta.description}</td>
   </tr>
   <tr>
     <th class="table-light">Date: </th>
-    <td colspan="3" class="table-light">${carta.date}</td>
+    <td class="table-light">${carta.date}</td>
   </tr>
   <tr>
     <th class="table-light">Event Category: </th>
-    <td colspan="3" class="table-light">${carta.category}</td>
+    <td class="table-light">${carta.category}</td>
   </tr>
   <tr>
     <th class="table-light">It takes place in: </th>
-    <td colspan="3" class="table-light">${carta.place}</td>
+    <td class="table-light">${carta.place}</td>
   </tr>
   <tr>
     <th class="table-light">How much would it cost? </th>
-    <td colspan="3" class="table-light">${carta.price} USD</td>
+    <td class="table-light">${carta.price} USD</td>
   </tr>
   </table>
-  
+
   <img src="${carta.image}" alt="Card image cap" class="imagenes-detalles">
 
   <p class="boton">
@@ -59,7 +59,21 @@ function createCard(carta, contenedor) {
     </p>
   `
     contenedor.appendChild(div)
+  }
+
+  createCard(cartas, contenedor)
+
+  }catch(error){
+      console.log(error)
+  }
 }
 
-createCard(cartas, contenedor)
+cargarEventos();
+
+
+
+$(document).ready(function(){
+  alert('Hello! I am Working!')
+});
+
 
--- upcoming.js
+++ upcoming.js
@@ -1,42 +1,40 @@
-$(document).ready(function(){
-    alert('Hello! I am Working!')
-});
+const cargarEventos = async() => {
+    try {
+        const respuesta = await fetch('https://mindhub-xj03.onrender.com/api/amazing');
+        //const respuesta = await fetch('data.json');
+        console.log(respuesta);
 
+        const datos = await respuesta.json();
+        console.log(datos);
 
-let contenedor = document.getElementById("contenedor-cartas");
+        ////mostrar cartas
+        let contenedor = document.getElementById("contenedor-cartas");
 
+        mostrarCarta(datos.events, contenedor)
 
-mostrarCarta(data.events, contenedor)
-
-function mostrarCarta(listCards, contenedor) {
-    contenedor.innerHTML = ''
-    if (listCards.length > 0) {
-
+        function mostrarCarta(listCards, contenedor) {
+        contenedor.innerHTML = ''
         let fragment = document.createDocumentFragment()
+        fragment.className = "contenedor"
         for (let element of listCards) {
-            if(element.date > data.currentDate) {
-            let divi = document.createElement('div')
-            divi.classList.add("carta")
-            divi.style.padding = "1%"
-            divi.innerHTML = 
-            `<img src="${element.image}" class="card-img-top"></img> <h5 class="card-title list-group-item">${element.name}</h5> <p class="card-text list-group-item">${element.description}</p> <p class="card-text list-group-item">${element.date}</p> <p class="card-text list-group-item">${element.category}</p> <p class="card-text list-group-item">${element.price} USD</p> <a href="./details.html?id=${element._id}" class="card-link btn btn-secondary">Details</a>`
-            fragment.appendChild(divi)
-            }
+            if(element.date > datos.currentDate) {
+                let divi = document.createElement('div')
+                divi.classList.add("carta")
+                divi.style.padding = "1%"
+                divi.innerHTML = 
+                `<img src="${element.image}" class="card-img-top"></img> <h5 class="card-title list-group-item">${element.name}</h5> <p class="card-text list-group-item">${element.description}</p> <p class="card-text list-group-item">${element.date}</p> <p class="card-text list-group-item">${element.category}</p> <p class="card-text list-group-item">${element.price} USD</p> <a href="./details.html?id=${element._id}" class="card-link btn btn-secondary">Details</a>`
+                fragment.appendChild(divi)
+                }
         }
         contenedor.appendChild(fragment)
-    } else {
-        let divi = document.createElement('div')
-        divi.innerHTML = `<p class="card-text">Not Found Category</p>`
-        contenedor.appendChild(divi)
-    }
-}
+        } 
 
+        //checkbox
+        const categorias = document.getElementById('prueba')
 
-const categorias = document.getElementById('prueba')
+        categorias.appendChild(checkbox(datos.events))
 
-    categorias.appendChild(checkbox(data.events))
-
-    function checkbox(array) {
+        function checkbox(array) {
 
         let arrayCategorias = []
 
@@ -59,49 +57,64 @@ const categorias = document.getElementById('prueba')
             fragmentCheck.appendChild(casillero)
         }
         return fragmentCheck
-    }
+        }
 
 
+        let chequeados = []
 
-
-    let chequeados = []
-
-    let checkboxes = document.querySelectorAll('input[type=checkbox]')
-    checkboxes.forEach(checkbox => {
+        let checkboxes = document.querySelectorAll('input[type=checkbox]')
+        checkboxes.forEach(checkbox => {
         checkbox.addEventListener('change', verificar)
-    })
+        })
 
-    function verificar() {
+        function verificar() {
         chequeados = Array.from(checkboxes).filter(checkbox => checkbox.checked).map(checkbox => checkbox.id)
         console.log(chequeados)
-        filtroCruzado(data.events)
-    }
-    function filterArray(arrayString, listCards) {
+        filtroCruzado(datos.events)
+        }
+
+        //barra y filtro cruzado
+
+        function filterArray(arrayString, listCards) {
         if (arrayString.length == 0) return listCards
         return listCards.filter(evento => arrayString.includes(evento.category.replace(" ", "_")))
-    }
+        }
 
-    let stringSearch = ""
+        let stringSearch = ""
 
-    const search_input = document.getElementById("barra-buscador")
-    console.log(search_input)
+        const search_input = document.getElementById("barra-buscador")
+        console.log(search_input)
 
-    search_input.addEventListener('keyup', () => {
+        search_input.addEventListener('keyup', () => {
         stringSearch = search_input.value
-        filtroCruzado(data.events)
-    })
+        filtroCruzado(datos.events)
+        })
 
-    function filterString(string, listCards) {
+        function filterString(string, listCards) {
         if (string == "") return listCards
         return listCards.filter(evento => evento.name.toLowerCase().includes(string.toLowerCase().trim()))
-    }
+        }
 
-    function filtroCruzado(listCards) {
+        function filtroCruzado(listCards) {
         let arrayFilterCheck = filterArray(chequeados, listCards)
         let arrayFilterString = filterString(stringSearch, arrayFilterCheck)
 
         mostrarCarta(arrayFilterString, contenedor)
+        }
+
+    }catch(error){
+        console.log(error)
     }
+}
+
+cargarEventos();
+
+
+$(document).ready(function(){
+    alert('Hello! I am Working!')
+});
+
+
 
 
 
